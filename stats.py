99434a0fd96e75f7b05e84fd0a99b679ace0bdc4
bmintz <bmintz@users.noreply.github.com>
2018-02-06 15:56:30 -0600

    stats: pretty much rewrite it
    bots.discordlist.net is no longer a thing, and the API for PW and DBL
    is exactly the same. This means we can just use one cog! The best part is,
    since there's only one cog, the stats command only gets registered once,
    which means that this cog can be reloaded without errors.

--- stats.py
+++ stats.py
@@ -14,7 +14,7 @@ which maps each domain name to either null or a token.
 """
 
 
-class StatsAPI:
+class Stats:
 	# credit to @Tom™#7887 (ID 248294452307689473) on the Discord Bots List guild
 	# for much of this
 	def __init__(self, bot):
@@ -22,37 +22,46 @@ class StatsAPI:
 		self.session = aiohttp.ClientSession(loop=bot.loop)
 		self.config = self.bot.config['tokens']['stats']
 
+		self.configured_apis = []
+		for config_key in ('bots.discord.pw', 'discordbots.org'):
+			if self.config[config_key] is None:
+				print(config_key, "was not loaded! Please make sure it's configured correctly.")
+			else:
+				self.configured_apis.append(config_key)
+
 	def __unload(self):
 		self.bot.loop.create_task(self.session.close())
 
-	async def send(self, url, headers={}, data={}):
-		"""send the statistics to the API gateway."""
-		async with self.session.post(url, data=data, headers=headers) as resp:
-			print('[STATS]', self.config_section, end=' ', file=sys.stderr)
-			if resp.status // 100 == 2:  # 2xx codes are success
-				print('response:', await resp.text(), file=sys.stderr)
-			else:
-				print('failed with status code', resp.status, file=sys.stderr)
+	async def send(self):
+		"""send guild counts to the API gateways."""
 
-	@property
-	def api_key(self):
-		return self.config[self.config_section]
+		for config_key in self.configured_apis:
+			url = 'https://{}/api/bots/{}/stats'.format(config_key, self.bot.user.id)
+			data = json.dumps({'server_count': len(self.bot.guilds)})
+			headers = {'Authorization': self.config[config_key], 'Content-Type': 'application/json'}
 
-	@property
-	def guild_count(self):
-		return len(self.bot.guilds)
+			async with self.session.post(url, data=data, headers=headers) as resp:
+				print('[STATS]', config_key, end=' ', file=sys.stderr)
+				if resp.status // 100 == 2:  # 2xx codes are success
+					print('response:', await resp.text(), file=sys.stderr)
+				else:
+					print('failed with status code', resp.status, file=sys.stderr)
 
 	@commands.command(name='stats', hidden=True)
 	@commands.is_owner()
 	async def send_command(self, context):
+		error = ''
 		try:
-			await self.send()
+			await cog.send()
 		except:
 			response = '\N{cross mark}'
+			error = 'Sending stats for %s failed!' % cog.config_section
+			print(error, file=sys.stderr)
 			print(traceback.format_exc(), file=sys.stderr)
 		else:
 			response = '\N{white heavy check mark}'
 		await context.message.add_reaction(response)
+		if error: await context.send(error)
 
 	async def on_ready(self):
 		await self.send()
@@ -64,46 +73,5 @@ class StatsAPI:
 		await self.send()
 
 
-class DiscordPWStats(StatsAPI):
-	config_section = 'bots.discord.pw'
-
-	async def send(self):
-		await super().send(
-			'https://bots.discord.pw/api/bots/%s/stats' % self.bot.user.id,
-			data=json.dumps({'server_count': self.guild_count}),
-			headers={
-				'Authorization': self.api_key,
-				'Content-Type': 'application/json'})
-
-
-class DiscordBotList(StatsAPI):
-	config_section = 'discordbots.org'
-
-	async def send(self):
-		await super().send(
-			'https://discordbots.org/api/bots/%s/stats' % self.bot.user.id,
-			data=json.dumps({'server_count': self.guild_count}),
-			headers={
-				'Authorization': self.api_key,
-				'Content-Type': 'application/json'})
-
-
-class Discordlist(StatsAPI):
-	config_section = 'bots.discordlist.net'
-
-	async def send(self):
-		await super().send(
-			'https://bots.discordlist.net/api',
-			data=json.dumps({
-				'token': self.api_key,
-				'servers': self.guild_count}),
-			headers={'Content-Type': 'application/json'})
-
-
 def setup(bot):
-	for Cog in (DiscordPWStats, DiscordBotList, Discordlist):
-		stats_config = bot.config['tokens']['stats']
-		if stats_config.get(Cog.config_section) is not None:
-			bot.add_cog(Cog(bot))
-		else:
-			print(Cog.config_section, "was not loaded! Please make sure it's configured properly.")
+	bot.add_cog(Stats(bot))
