97a834c2470107fae1543803d5a8886abddacf7d
bmintz <bmintz@users.noreply.github.com>
2018-04-01 15:34:00 -0500

    bump version

--- ben_cogs/__init__.py
+++ ben_cogs/__init__.py
@@ -4,7 +4,7 @@ __title__		= 'ben_cogs'
 __author__		= 'bmintz'
 __license__		= 'MIT'
 __copyright__ 	= 'Copyright © 2018 Benjamin Mintz'
-__version__ 	= '0.0.1'
+__version__ 	= '0.0.2'
 
 
 from . import admin, misc, stats
