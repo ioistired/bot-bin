08baaa76612a894099b6cd52088477f840c7603d
bmintz <bmintz@users.noreply.github.com>
2018-04-01 15:04:59 -0500

    pip stuff (closes #1)

---
+++ ben_cogs/__init__.py
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+__title__		= 'ben_cogs'
+__author__		= 'bmintz'
+__license__		= 'MIT'
+__copyright__ 	= 'Copyright © 2018 Benjamin Mintz'
+__version__ 	= '0.0.1'
+
+
+from . import admin, misc, stats
+
---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3.6
+# encoding: utf-8
+
+import re
+from setuptools import setup
+
+
+with open('requirements.txt') as f:
+	requirements = f.read().splitlines()
+
+
+with open('ben_cogs/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+
+setup(
+	name='ben_cogs',
+	author='bmintz',
+	url='https://github.com/bmintz/cogs',
+	version=version,
+	packages=['ben_cogs'],
+	install_requires=requirements,
+	python_requires='>=3.6.0',
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Framework :: AsyncIO',
+		'License :: OSI Approved :: MIT License',
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3.6',
+		'Topic :: Internet',
+		'Topic :: Utilities'])
