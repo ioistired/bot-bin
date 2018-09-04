ea262aa661a9480420d01ab3ff74a29043f4b801
bmintz <bmintz@protonmail.com>
2018-09-04 02:06:46 +0000

    bump version

--- ben_cogs/__init__.py
+++ ben_cogs/__init__.py
@@ -4,7 +4,7 @@ __title__		= 'ben_cogs'
 __author__		= 'bmintz'
 __license__		= 'MIT'
 __copyright__ 	= 'Copyright © 2018 Benjamin Mintz'
-__version__ 	= '0.0.17'
+__version__ 	= '0.0.18'
 
 
 from . import debug, misc, stats
