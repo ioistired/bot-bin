39f5ebe9926e2114782dcd45560491d60bd95710
bmintz <bmintz@protonmail.com>
2018-10-02 07:15:37 +0000

    add import expression jishaku

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ from setuptools import setup
 with open('ben_cogs/__init__.py') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
+with open('requirements.txt') as f:
+	dependency_links = [line for line in f if line and not line.lstrip().startswith('#')]
 
 setup(
 	name='ben_cogs',
@@ -17,9 +19,12 @@ setup(
 	download_url='https://github.com/bmintz/cogs/archive/{}.tar.gz'.format(version),
 	version=version,
 	packages=['ben_cogs'],
+	dependency_links=dependency_links,
 	install_requires=[
 		'discord.py>=1.0.0a1430',
 		'humanize',
+		'import_expression<1.0.0',
+		'jishaku',
 		'objgraph',
 		'psutil'],
 	python_requires='>=3.6.0',
