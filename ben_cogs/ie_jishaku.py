7ca9c450d7c625f34756597e248c5ad1480f17bc
bmintz <bmintz@protonmail.com>
2018-11-03 10:10:40 +0000

    fix jsk py help(x)

--- ben_cogs/ie_jishaku.py
+++ ben_cogs/ie_jishaku.py
@@ -18,7 +18,7 @@ class ImportExpressionJishaku(jishaku.cog.Jishaku):
 			'asyncio': asyncio,
 			'discord': discord,
 			'commands': discord.ext.commands,
-			'help': lambda x: '```'+pydoc.render_doc+'```',
+			'help': lambda x: '```'+pydoc.render_doc(x)+'```',
 
 			import_expression.constants.IMPORTER: importlib.import_module,
 		}
