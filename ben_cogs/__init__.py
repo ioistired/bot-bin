231eec4541e170da1d495c74837112e488c36e53
Benjamin Mintz <bmintz@protonmail.com>
2019-06-20 05:23:22 +0000

    bump minor version

--- ben_cogs/__init__.py
+++ ben_cogs/__init__.py
@@ -2,6 +2,6 @@ __title__		= 'ben_cogs'
 __author__		= 'bmintz'
 __license__		= 'MIT'
 __copyright__ 	= 'Copyright © 2018–2019 Benjamin Mintz'
-__version__ 	= '0.12.1'
+__version__ 	= '0.13.0'
 
 from .bot import BenCogsBot
