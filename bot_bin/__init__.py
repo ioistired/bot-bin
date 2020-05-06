94b2b249a53a23a433afc0a6ff147d18f5d22a79
io mintz <io@mintz.cc>
2020-05-06 17:44:53 +0000

    bump minor version to 1.3.0

--- bot_bin/__init__.py
+++ bot_bin/__init__.py
@@ -1,5 +1,5 @@
 __title__		= 'bot_bin'
 __author__		= 'lambda#0987'
 __license__		= 'BlueOak-1.0.0'
-__copyright__ 	= 'Copyright © 2018–2019 lambda#0987'
-__version__ 	= '1.2.1'
+__copyright__ 	= 'Copyright © 2018–2020 lambda#0987'
+__version__ 	= '1.3.0'
